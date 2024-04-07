01628c566a7a4a35ccc89ce5a268e853389783f2
David wanjohi <davidwagura83@gmail.com>
2024-04-07 20:14:24 +0300

    Create controller

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function index()
+    {
+        $companies = Company::orderBy('id', 'desc')->paginate(5);
+        return view ('companies.index', compact('companies'));
+    }
+
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required'
+        ]);
+
+        Company::create($request->post());
+
+        return redirect()->route('companies.index')->with('success', 'Company has been created successfully.');
+    }
+
+    public function update(Request $request, Company $company)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+        ]);
+
+        $company->fill($request->post())->save();
+
+        return redirect()->route('companies.index')->with('success', 'Company Has Been updated successfully');
+    }
+
+    public function destroy(Company $company)
+    {
+        $company->delete();
+
+        return redirect()->route('companies.index')->with('success', 'Company has been deleted successfully');
+    }
+}
