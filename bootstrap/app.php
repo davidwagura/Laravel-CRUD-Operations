f50e57d431f414fa28533885f2b8577412dc199c
David wanjohi <davidwagura83@gmail.com>
2024-06-19 13:00:51 +0300

    Add vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create()
+    
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    });
+    
